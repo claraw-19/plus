edf0243b949863958ea443311e228c6b3bde06c4
Clara Weyer <clara.weyer@schullv.de>
2025-01-08 14:29:03 +0100

    map over users to get list with names

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,17 @@
+import users from "../constants/dummyData.json";
+
 export default function Plus() {
-  return "Hello World";
+  console.log("users: ", users);
+  return (
+    <>
+      <h1>PLUS-Lizenzen</h1>
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}>
+            {user.firstName} {user.lastName}
+          </li>
+        ))}
+      </ul>
+    </>
+  );
 }
