7d3e1150f1c9f562fd6561ce2748682048cb4c01
Clara Weyer <clara.weyer@schullv.de>
2025-01-08 15:14:51 +0100

    improve styling of list

--- components/UserCard.tsx
+++ components/UserCard.tsx
@@ -1,20 +1,25 @@
 import styled from "styled-components";
 
 export default function UserCard({ user }) {
-  const CardContainer = styled.div`
+  const UserContainer = styled.div`
     display: flex;
     justify-content: space-between;
     padding: 8px 16px;
     border-bottom: 1px solid #ccc;
   `;
 
+  const UserData = styled.p`
+    flex: 1;
+  `;
+
   return (
-    <CardContainer>
-      <p>
+    <UserContainer>
+      <UserData>
         {user.firstName} {user.lastName}
-      </p>
-      <p>{user.accessCodesId}</p>
-      <p>{user.nextPaymentDate ? user.nextPaymentDate : "------"}</p>
-    </CardContainer>
+      </UserData>
+      <UserData>{user.email}</UserData>
+      <UserData>{user.accessCodesId}</UserData>
+      <UserData>{user.nextPaymentDate}</UserData>
+    </UserContainer>
   );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,24 @@
 import users from "../constants/dummyData.json";
 import UserCard from "@/components/UserCard";
+import styled from "styled-components";
 
 export default function Plus() {
-  console.log("users: ", users);
+  const UserList = styled.ul`
+    list-style: none;
+    margin: 0;
+    padding: 0;
+  `;
+
   return (
     <>
       <h1>PLUS-Lizenzen</h1>
-      <ul>
+      <UserList>
         {users.map((user) => (
           <li key={user.id}>
             <UserCard user={user} />
           </li>
         ))}
-      </ul>
+      </UserList>
     </>
   );
 }
