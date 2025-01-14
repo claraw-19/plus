b5ce5a35bc0f5fa71c8b9568776d84a581e6e9df
Clara Weyer <clara.weyer@schullv.de>
2025-01-14 15:59:17 +0100

    add monthly paying volume kpi

--- components/Plus/Kpi.jsx
+++ components/Plus/Kpi.jsx
@@ -1,15 +1,23 @@
 import styled from "styled-components";
+import NumbersIcon from "@mui/icons-material/Numbers";
+import CreditScoreIcon from "@mui/icons-material/CreditScore";
 
 export default function KPI({ users }) {
   const Styled = {
     KpiWrapper: styled.div`
       display: flex;
-      flex-direction: row;
-      align-items: center;
-      padding: 0.5rem 1rem;
       border-radius: 0.5rem;
       width: 100%;
       background-color: ${({ theme }) => theme.colors.schullv4};
+      justify-content: space-between;
+      align-items: center;
+      padding: 10px;
+    `,
+
+    IconTextWrapper: styled.div`
+      display: flex;
+      align-items: center;
+      gap: 8px;
     `,
   };
   const totalUsers = users.length;
@@ -18,9 +26,26 @@ export default function KPI({ users }) {
     (user) => user.status === "active"
   ).length;
 
+  const payingVolumeMonthly = users.reduce((total, user) => {
+    if (user.status === "active") {
+      const price = parseFloat(user.price.replace(",", "."));
+      return total + price;
+    }
+    return total;
+  }, 0);
+
   return (
-    <Styled.KpiWrapper>
-      {totalActiveUsers} / {totalUsers}
-    </Styled.KpiWrapper>
+    <>
+      <Styled.KpiWrapper>
+        <Styled.IconTextWrapper>
+          <NumbersIcon />
+          {totalActiveUsers} / {totalUsers}
+        </Styled.IconTextWrapper>
+        <Styled.IconTextWrapper>
+          <CreditScoreIcon />
+          {Math.round(payingVolumeMonthly * 100) / 100} €
+        </Styled.IconTextWrapper>
+      </Styled.KpiWrapper>
+    </>
   );
 }
