f5ccb1a2381e1f9e2e5d8963d118ea2ac4c55b45
Clara Weyer <clara.weyer@schullv.de>
2025-01-17 12:48:33 +0100

    add popup

--- components/Plus/Filter.jsx
+++ components/Plus/Filter.jsx
@@ -1,8 +1,11 @@
-import FilterListIcon from "@mui/icons-material/FilterList";
+import PlaylistAddIcon from "@mui/icons-material/PlaylistAdd";
+import { useState } from "react";
 import styled from "styled-components";
+import Modal from "@mui/material/Modal";
+import Box from "@mui/material/Box";
 
 const Styled = {
-  FilterListIcon: styled(FilterListIcon)`
+  PlaylistAddIcon: styled(PlaylistAddIcon)`
     font-size: 2rem;
     padding: 10px;
 
@@ -12,8 +15,40 @@ const Styled = {
       background-color: ${({ theme }) => theme.colors.grey7};
     }
   `,
+
+  ModalContent: styled(Box)`
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    width: 400px;
+    background-color: white;
+    box-shadow: 24px;
+    padding: 20px;
+    border-radius: 8px;
+  `,
 };
 
 export default function Filter() {
-  return <Styled.FilterListIcon />;
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleAddFilter = () => {
+    setIsOpen(true);
+  };
+
+  const handleClose = () => {
+    setIsOpen(false);
+  };
+
+  return (
+    <>
+      <Styled.PlaylistAddIcon onClick={handleAddFilter} />
+      <Modal open={isOpen} onClose={handleClose}>
+        <Styled.ModalContent>
+          <h2>Popup geöffnet!</h2>
+          <button onClick={handleClose}>Schließen</button>
+        </Styled.ModalContent>
+      </Modal>
+    </>
+  );
 }
