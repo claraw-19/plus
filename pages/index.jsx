c28612821246903a54a8a2ddd59c031aa72cda52
Clara Weyer <clara.weyer@schullv.de>
2025-01-22 08:19:55 +0100

    add starts with filter

--- pages/index.jsx
+++ pages/index.jsx
@@ -131,6 +131,15 @@ export default function Plus() {
           return !fieldValue.toLowerCase().includes(filter.value.toLowerCase());
         });
       }
+      if (filter.filterMethod.id === "startsWith") {
+        filterResult = filterResult.filter((singleOrder) => {
+          const fieldValue =
+            singleOrder[filter.field.object][filter.field.name];
+          return fieldValue
+            .toLowerCase()
+            .startsWith(filter.value.toLowerCase());
+        });
+      }
       console.log("result: ", filterResult);
       console.log("filterline: ", filter);
     }
